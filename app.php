2e86e67fd4feaaf01fb6a59469623f96832e32c9
Fernando Nunes <gnomonunes@gmail.com>
2017-07-08 12:18:57 +0100

    Update app running

--- app.php
+++ app.php
@@ -3,16 +3,24 @@ declare(strict_types=1);
 
 require_once 'autoloader.php';
 
+use \App\Classes\Behaviors\FlyRocketPowered;
+
 $ducks = array(
     'MallardDuck',
     'RedheadDuck',
-    'RubberDuck'
+    'RubberDuck',
+    'ModelDuck'
 );
 
 foreach ($ducks as $duckBreed) {
     $className = "App\Classes\Entities\\{$duckBreed}";
 
     $duck = new $className();
+
+    if ($duckBreed == 'ModelDuck') {
+        $duck->setFlyBehavior(new FlyRocketPowered());
+    }
+
     $duck->display();
     $duck->performQuack();
     $duck->performFly();
